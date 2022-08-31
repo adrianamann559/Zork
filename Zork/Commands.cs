c27f92a4bb74b76f210f7995d1b20d53c91068dd
adrianamann559 <amann559@knights.ucf.edu>
2022-08-31 12:35:22 -0400

    zork 1.2
    note to self: Still has warnings, trying to figure it out

---
+++ Zork/Commands.cs
@@ -0,0 +1,14 @@
+﻿namespace Zork
+{
+    public enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+}
+   
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,17 +2,7 @@
 
 namespace Zork
 {
-
-    enum Commands
-    {
-        QUIT,
-        LOOK,
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST,
-        UNKNOWN
-    }
+    
     class Program
     {
         static void Main(string[] args)
@@ -20,20 +10,83 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine().Trim().ToUpper();
-         
-
-            if (inputString == "QUIT")
+            Commands command = ToCommand(inputString);
+            if (command == Commands.QUIT)
             {
-                Console.WriteLine("Thanks for playing!");
+                Console.WriteLine("Thanks you for playing.");
             }
-            else if (inputString == "LOOK") 
+            else if (command == Commands.LOOK)
             {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                 Console.WriteLine("This is an open field west of a white house, with a boarded up front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else if (command == Commands.NORTH)
+            {
+                Console.WriteLine("You moved north.");
+            }
+            else if (command == Commands.SOUTH)
+            {
+                Console.WriteLine("You moved south.");
+            }
+            else if (command == Commands.EAST)
+            {
+                Console.WriteLine("You moved east.");
+            }
+            else if (command == Commands.WEST)
+            {
+                Console.WriteLine("You moved west.");
             }
             else
             {
-                Console.WriteLine($"Unrecognized command: {inputString}");
+              Console.WriteLine($"Unrecognized command: {inputString}");
             }
         }
+
+        static Commands ToCommand(string commandString)
+        {
+            if (Enum.TryParse<Commands>(commandString, true, out Commands command))
+            {
+                return command;
+            }
+            else
+            {
+                return Commands.UNKNOWN;
+            }
+
+            switch (commandString)
+            {
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
+
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+
+                default:
+                    command = Commands.UNKNOWN;
+                    break;
+            }
+
+           
+        }
+      
+         
+       
     }
 }
