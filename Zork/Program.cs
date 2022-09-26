f8262e7fa69499e0d67132cd3504bbdc00269835
adrianamann559 <amann559@knights.ucf.edu>
2022-09-26 14:21:09 -0400

    zork 3.2 progress

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -18,6 +18,7 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
+            Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
@@ -50,9 +51,13 @@ namespace Zork
                         break;
                 }
             }
+            if (previousRoom != CurrentRoom)
+            {
+                Console.WriteLine(CurrentRoom.Description);
+                previousRoom = CurrentRoom;
+            }
 
         }
-
         private static bool Move(Commands command)
         {
             Assert.IsTrue(IsDirection(command), "Invalid direction.");
@@ -93,20 +98,35 @@ namespace Zork
             {new Room("Forest"), new Room ("West of House"), new Room ("Behind House")},
             {new Room("Dense Woods"), new Room("North of House"), new Room ("Clearing")}
         };
-
         private static void IntializeRoomDescriptions()
         {
-            Rooms[0, 0].Description = "You are on a rock-strewn trail."; //Rocky Trail
-            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred"; //South of House
-            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall."; //Canyon View
+            var roomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
+            roomMap[""].Description = "";
 
-            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you."; //Forest
-            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door."; //West of House
-            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house, there is a small window which is slightly ajar."; //Behind House
 
-            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; //Dense Woods
-            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; //North of House
-            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south."; //Clearing
+            //Rooms[0, 0].Description = "You are on a rock-strewn trail."; //Rocky Trail
+            //Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred"; //South of House
+            //Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall."; //Canyon View
+
+            //Rooms[1, 0].Description = "This is a forest, with trees in all directions around you."; //Forest
+            //Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door."; //West of House
+            //Rooms[1, 2].Description = "You are behind the white house. In one corner of the house, there is a small window which is slightly ajar."; //Behind House
+
+            //Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; //Dense Woods
+            //Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; //North of House
+            //Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south."; //Clearing
         }
 
         private static readonly List<Commands> Directions = new List<Commands>
